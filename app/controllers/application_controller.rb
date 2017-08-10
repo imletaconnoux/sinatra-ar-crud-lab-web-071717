238c35515fa130d45b50eea9c0c7ff58bafa12fc
imletaconnoux <imletaconnoux@gmail.com>
2017-08-10 15:49:38 -0400

    passed tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,61 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+
   end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  post '/posts' do
+    @post = Post.new(name: params[:name], content: params[:content])
+    @post.save
+    redirect '/posts'
+  end
+
+  get '/posts/new' do
+
+    erb :new
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+
+    @post = Post.find_by(id: params[:id])
+
+    @post.update(name: params[:name], content: params[:content])
+
+
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
+
+  get '/posts/:id/delete' do
+
+  
+  end
+
+
 end
